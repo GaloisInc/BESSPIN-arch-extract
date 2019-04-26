4212d92b385a476b41cc44854fed8dd7d5dae446
Stuart Pernsteiner <spernsteiner@galois.com>
2019-04-26 10:29:59 -0700

    use `driver check-config` in piccolo example

--- examples/piccolo/check-config.sh
+++ examples/piccolo/check-config.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 set -e
 
-work=$(dirname "$0")
-inst=$1
+work="$(realpath "$(dirname "$0")")"
+config=$(realpath $1)
+inst=$2
 
 mkdir -p $work/logs
 exec 2>$work/logs/$(date +%Y%m%d-%H%M%S)-$$-$inst.log 1>&2
 
-piccolo=$PWD/../gfe/bluespec-processors/P1/Piccolo
-
-inst_dir=$work/$inst
-
-if [ ! -d $inst_dir ]; then
-    echo "initialize $inst_dir"
-    mkdir $inst_dir
-    cp -a $piccolo/{src_Core,src_Testbench} $inst_dir/
-    mkdir -p $inst_dir/build
-fi
-
-cd $inst_dir/build
-# stdin of this script is passed through to build.sh
-exec "$work/build.sh" "$piccolo"
+./driver $config check-config \
+    $(grep '^1 ' | sed -e 's/^1 //')
