cf230a54a6621647391fb4c2ae19ddb959dacf8d
Stuart Pernsteiner <spernsteiner@galois.com>
2019-07-12 13:30:20 -0700

    update rocket-p1 featuresynth config

--- examples/rocket-p1/check-config.sh
+++ examples/rocket-p1/check-config.sh
@@ -6,5 +6,16 @@ work="$(realpath "$(dirname "$0")")"
 mkdir -p $work/logs
 exec 2>$work/logs/$(date +%Y%m%d-%H%M%S)-$$.log 1>&2
 
+export BESSPIN_CHECK_CONFIG_CACHE_DIR=$work/check-config-cache
+
 python3 rocket-chip-check-config/check.py check \
-    $(grep '^1 ' | sed -e 's/^1 //')
+    $(grep '^1 ' | sed -e 's/^1 //') \
+    -- \
+    'galois.subsystem.WithL1ICacheSets(64)' \
+    'galois.subsystem.WithL1DCacheSets(64)' \
+    'galois.subsystem.WithNSmallCores(1)' \
+    'galois.subsystem.WithEdgeDataBits(64)' \
+    'galois.subsystem.WithNExtTopInterrupts(16)' \
+    'galois.subsystem.WithDTS("galois,rocketchip-p1", Nil)' \
+    'galois.subsystem.WithTimebase(BigInt(100000000))' \
+    'galois.subsystem.BaseSubsystemConfig'
--- examples/rocket-p1/init-tests.rktd
+++ examples/rocket-p1/init-tests.rktd
@@ -1,1 +1,14 @@
-()
+(
+    galois.subsystem.WithGFEMemPort
+    galois.subsystem.WithGFEMMIOPort
+    galois.subsystem.WithoutTLMonitors
+    galois.subsystem.WithGFECLINT
+    galois.subsystem.WithRV32
+)
+
+(
+    galois.subsystem.WithDefaultMemPort
+    galois.subsystem.WithDefaultMMIOPort
+    galois.subsystem.WithoutTLMonitors
+    galois.subsystem.WithRV32
+)
